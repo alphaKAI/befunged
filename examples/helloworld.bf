bfdfc5c8ed26fb0b5763cb41c1367ac0786466d5
alphaKAI <alpha.kai.net@alpha-kai-net.info>
2017-04-10 21:31:48 +0900

    add examples into examples directory

---
+++ examples/helloworld.bf
@@ -0,0 +1,5 @@
+v @_       v
+>0"!dlroW"v 
+v  :#     < 
+>" ,olleH" v
+   ^       <
--- source/app.d
+++ source/app.d
@@ -1,13 +1,30 @@
 module app;
 import befunged.interpreter;
+import std.stdio,
+       std.file;
 
-void main() {
-  string src = 
-`5 100p:v
-v *g00:_00g.@
->00p1-:^`;
+void main(string[] args) {
+  if (args.length >= 2) {
+    string fpath = args[1];
 
-  Interpreter itpr = new Interpreter(src);
+    if (!exists(fpath)) {
+      writeln("No such file - ", fpath);
+    } else {
+      Interpreter itpr = new Interpreter(readText(fpath));
 
-  itpr.execute;
+      itpr.execute;
+    }
+  } else if (args.length == 1) {
+    string code,
+           line;
+
+    while ((line = readln()) !is null) {
+      code ~= line;
+    }
+
+    Interpreter itpr = new Interpreter(code);
+    itpr.execute();
+  } else {
+    writeln("error"); // this error comment needs improving
+  }
 }
